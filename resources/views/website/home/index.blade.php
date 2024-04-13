c496a88e50d40944661c93c6753b18007ceae710
ahmadshahfarhan <98960641+ahmadshahfarhan@users.noreply.github.com>
2024-04-13 07:04:31 +0700

    upadate data dummy

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -27,58 +27,67 @@ integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xX
                 @csrf
                 <div class="form-section">
                     <h2 class=" text-black text-[32px] weigth text-center mt-4">What's your home address? *</h2>
-                    <div class="progress-bar flex gap-1 items-center mx-auto justify-center mt-6">
-                        <div class="bg-[#F3F4F6] step step0 w-[70.15px] h-[6px] rounded"></div>
-                        <div class="bg-[#F3F4F6] step step1 w-[70.15px] h-[6px] rounded"></div>
-                        <div class="bg-[#F3F4F6] step step2 w-[70.15px] h-[6px] rounded"></div>
+                @section('progres')
+                    @include('website.progres')
+                @show
+
+                <div class=" flex flex-wa justify-center gap-[32px] mt-[56px]">
+                    <div>
+                        <h1 class="text-black text-[18px] weigth text-center mb-[12px]" for="">What's your email
+                            address? *</h1>
+                        <input class="w-[360px] outline-none border-[1px] border-[#E5E7EB] rounded-lg p-2 placeholder-1"
+                            type="email" name="name" placeholder="first name" required>
                     </div>
-                    <h2 class="text-gray-300 mt-6 mb-[48px] text-center">
-                        Your address is required to provide accurate results.
-                    </h2>
-                    <div
-                        class=" bg-white border-[1px] border-[#E5E7EB] shadow-sm flex items-center p-3 gap-2 rounded-lg lg:w-[400px] md:w-[400px] w-full mx-auto">
-                        <i class="fas fa-envelope"></i>
-                        <input class="w-[400px] outline-none border-none" placeholder="you@email.com" type="email"
-                            name="email" id="" required>
+                    <div>
+                        <h1 class="text-black text-[18px] weigth text-center mb-[12px]" for="">What's
+                            your mobile phone number? *</h1>
+                        <input
+                            class=" w-[360px] outline-none border-[1px] border-[#E5E7EB] rounded-lg p-2 placeholder-1"
+                            type="phone" name="last_name" placeholder="lass name" required>
                     </div>
                 </div>
+            </div>
 
-                <div class="form-section">
-                    <div class="progress-bar flex gap-1 items-center mx-auto justify-center mt-6 mb-[56px]">
-                        <div class="bg-[#F3F4F6] step step0 w-[70.15px] h-[6px] rounded"></div>
-                        <div class="bg-[#F3F4F6] step step1 w-[70.15px] h-[6px] rounded"></div>
-                        <div class="bg-[#F3F4F6] step step2 w-[70.15px] h-[6px] rounded"></div>
+            <div class="form-section">
+                {{-- progress bar manual --}}
+                {{-- <div class="progress-bar flex gap-1 items-center mx-auto justify-center mt-6 mb-[56px]">
+                    <div class="bg-[#F3F4F6] step step0 w-[70.15px] h-[6px] rounded"></div>
+                    <div class="bg-[#F3F4F6] step step1 w-[70.15px] h-[6px] rounded"></div>
+                    <div class="bg-[#F3F4F6] step step2 w-[70.15px] h-[6px] rounded"></div>
+                </div> --}}
+                @section('progres')
+                    @include('website.progres')
+                @show
+                <div class=" flex justify-center gap-[32px]">
+                    <div>
+                        <h1 class="text-black text-[18px] weigth text-center mb-[12px]" for="">What's your first
+                            name? *</h1>
+                        <input class="w-[360px] outline-none border-[1px] border-[#E5E7EB] rounded-lg p-2 placeholder-1"
+                            type="text" name="name" placeholder="first name" required>
                     </div>
-                    <div class=" flex justify-center gap-[32px]">
-                        <div>
-                            <h1 class="text-black text-[18px] weigth text-center mb-[12px]" for="">What's your first
-                                name? *</h1>
-                            <input class="w-[360px] outline-none border-[1px] border-[#E5E7EB] rounded-lg p-2 placeholder-1"
-                                type="text" name="name" placeholder="first name" required>
-                        </div>
-                        <div>
-                            <h1 class="text-black text-[18px] weigth text-center mb-[12px]" for="">What's your last
-                                name? *</h1>
-                            <input
-                                class=" w-[360px] outline-none border-[1px] border-[#E5E7EB] rounded-lg p-2 placeholder-1"
-                                type="text" name="last_name" placeholder="lass name" required>
-                        </div>
+                    <div>
+                        <h1 class="text-black text-[18px] weigth text-center mb-[12px]" for="">What's your last
+                            name? *</h1>
+                        <input
+                            class=" w-[360px] outline-none border-[1px] border-[#E5E7EB] rounded-lg p-2 placeholder-1"
+                            type="text" name="last_name" placeholder="lass name" required>
                     </div>
                 </div>
+            </div>
 
 
-                <div class="mt-[60px] mx-auto justify-center flex gap-5 form-navigation">
-                    <button class=" text-[#1F2937] text-[17px] weigth previous"><i class="fas fa-arrow-left"></i>
-                        PREVOUS</button>
-                    <button class=" next-button text-white bg-[#111827] rounded-lg text-[17px] weigth next">NEXT <i
-                            class="fas fa-arrow-right text-sm font-weight"></i></button>
-                    {{-- <button type="submit"
+            <div class="mt-[60px] mx-auto justify-center flex gap-5 form-navigation">
+                <button class=" text-[#1F2937] text-[17px] weigth previous"><i class="fas fa-arrow-left"></i>
+                    PREVOUS</button>
+                <button class=" next-button text-white bg-[#111827] rounded-lg text-[17px] weigth next">NEXT <i
+                        class="fas fa-arrow-right text-sm font-weight"></i></button>
+                {{-- <button type="submit"
                         class=" text-white bg-[#111827] p-3 rounded-lg text-[17px] weigth next">Submit</button> --}}
-                </div>
+            </div>
 
-            </form>
+        </form>
 
-            {{-- <div class=" mt-[150px]">
+        {{-- <div class=" mt-[150px]">
                 <h2 class="text-black text-[28px] text-center" style="font-weight: 700;">Why Use Solar Selector?</h2>
                 <div class=" mt-[80px] flex flex-wrap gap-[32px] justify-center">
                     <div class=" card-text card-near-footer w-[410.67px] p-4">
@@ -107,6 +116,6 @@ integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xX
                     </div>
                 </div>
             </div> --}}
-        </div>
     </div>
+</div>
 @stop
